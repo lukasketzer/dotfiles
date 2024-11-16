723a6d277742422aed9fd7639c6121ee7e0fc199
Lukas Ketzer <lkskbs8169782xqcfx@gmail.com>
2024-11-16 15:21:56 +0100

    updated dotfiles

--- nvim_new/.config/nvim/lua/options.lua
+++ nvim_new/.config/nvim/lua/options.lua
@@ -62,4 +62,12 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+-- spaces instead of tabs
+vim.opt.shiftwidth = 4
+vim.opt.smarttab = true
+vim.opt.expandtab = true
+
+-- make tab characters a differen size to differ them from spaces
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 0
 -- vim: ts=2 sts=2 sw=2 et
